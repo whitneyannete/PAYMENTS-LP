af6345542e6113ee5d4a787c59f3f4c8529c648e
whitneyannete <whitneyannete254@gmail.com>
2024-04-02 16:41:58 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        dm_sans: ['DM Sans', 'sans-serif'],
+        mulish: ['Mulish', 'sans-serif'],
+
+    },
+    },
+  },
+  plugins: [],
+}
+
